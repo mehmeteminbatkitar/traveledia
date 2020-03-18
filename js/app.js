633928ce03731d0db583a3d82ebe0522381caf21
Mehmet Emin Batkitar <49765648+mehmeteminbatkitar@users.noreply.github.com>
2020-03-19 02:06:05 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,186 @@
+/* MENÜ*/
+
+
+(function($) {
+
+    "use strict";
+    
+    var cfg = {
+        scrollDuration : 800, // smoothscroll duration
+        mailChimpURL   : 'https://facebook.us8.list-manage.com/subscribe/post?u=cdb7b577e41181934ed6a6a44&amp;id=e6957d85dc'   // mailchimp url
+    },
+
+    $WIN = $(window);
+
+    // Add the User Agent to the <html>
+    // will be used for IE10 detection (Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0))
+    var doc = document.documentElement;
+    doc.setAttribute('data-useragent', navigator.userAgent);
+
+
+   /* Preloader
+    * -------------------------------------------------- */
+    var clPreloader = function() {
+        
+        $("html").addClass('ss-preload');
+
+        $WIN.on('load', function() {
+
+            //force page scroll position to top at page refresh
+            // $('html, body').animate({ scrollTop: 0 }, 'normal');
+
+            // will first fade out the loading animation 
+            $("#loader").fadeOut("slow", function() {
+                // will fade out the whole DIV that covers the website.
+                $("#preloader").delay(300).fadeOut("slow");
+            }); 
+       
+        });
+    };
+ 
+
+   /* pretty print
+    * -------------------------------------------------- */ 
+    var clPrettyPrint = function() {
+        $('pre').addClass('prettyprint');
+        $( document ).ready(function() {
+            prettyPrint();
+        });
+    };
+
+
+   /* search
+    * ------------------------------------------------------ */
+    var clSearch = function() {
+            
+        var searchWrap = $('.header__search'),
+            searchField = searchWrap.find('.search-field'),
+            closeSearch = searchWrap.find('.header__overlay-close'),
+            searchTrigger = $('.header__search-trigger'),
+            siteBody = $('body');
+
+
+        searchTrigger.on('click', function(e) {
+            
+            e.preventDefault();
+            e.stopPropagation();
+        
+            var $this = $(this);
+        
+            siteBody.addClass('search-is-visible');
+            setTimeout(function(){
+                searchWrap.find('.search-field').focus();
+            }, 100);
+        
+        });
+
+        closeSearch.on('click', function(e) {
+
+            var $this = $(this);
+        
+            e.stopPropagation(); 
+        
+            if(siteBody.hasClass('search-is-visible')){
+                siteBody.removeClass('search-is-visible');
+                setTimeout(function(){
+                    searchWrap.find('.search-field').blur();
+                }, 100);
+            }
+        });
+
+        searchWrap.on('click',  function(e) {
+            if( !$(e.target).is('.search-field') ) {
+                closeSearch.trigger('click');
+            }
+        });
+            
+        searchField.on('click', function(e){
+            e.stopPropagation();
+        });
+            
+        searchField.attr({placeholder: 'Type Keywords', autocomplete: 'off'});
+
+    };
+
+
+   /* Mobile Menu
+    * ---------------------------------------------------- */ 
+   var clMobileMenu = function() {
+
+        var navWrap = $('.header__nav-wrap'),
+            closeNavWrap = navWrap.find('.header__overlay-close'),
+            menuToggle = $('.header__toggle-menu'),
+            siteBody = $('body');
+        
+        menuToggle.on('click', function(e) {
+            var $this = $(this);
+
+            e.preventDefault();
+            e.stopPropagation();
+            siteBody.addClass('nav-wrap-is-visible');
+        });
+
+        closeNavWrap.on('click', function(e) {
+            
+            var $this = $(this);
+            
+            e.preventDefault();
+            e.stopPropagation();
+        
+            if(siteBody.hasClass('nav-wrap-is-visible')) {
+                siteBody.removeClass('nav-wrap-is-visible');
+            }
+        });
+
+        // open (or close) submenu items in mobile view menu. 
+        // close all the other open submenu items.
+        $('.header__nav .has-children').children('a').on('click', function (e) {
+            e.preventDefault();
+
+            if ($(".close-mobile-menu").is(":visible") == true) {
+
+                $(this).toggleClass('sub-menu-is-open')
+                    .next('ul')
+                    .slideToggle(200)
+                    .end()
+                    .parent('.has-children')
+                    .siblings('.has-children')
+                    .children('a')
+                    .removeClass('sub-menu-is-open')
+                    .next('ul')
+                    .slideUp(200);
+
+            }
+        });
+    };
+
+
+
+
+   /* Initialize
+    * ------------------------------------------------------ */
+    (function clInit() {
+
+        clPreloader();
+        clPrettyPrint();
+        clSearch();
+        clMobileMenu();
+        clSlickSlider();
+        clSmoothScroll();
+        clAlertBoxes();
+        clAOS();
+        clAjaxChimp();
+        clBackToTop();
+        clGoogleMap();
+
+    })();
+
+    
+
+})(jQuery);
+
+
+    /* ===================================================================
+     * #MENÜ END
+     *
+     * ------------------------------------------------------------------- */
